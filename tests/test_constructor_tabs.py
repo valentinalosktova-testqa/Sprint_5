237919235f7afbfe68f319b01f0ac3d0ffa13443
Valentina Loskutova <valentina.losktova@gmail.com>
2026-06-16 00:24:48 +0300

    refactor: wrap constructor tabs tests in TestConstructorTabs class

--- tests/test_constructor_tabs.py
+++ tests/test_constructor_tabs.py
@@ -4,17 +4,17 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
-@pytest.mark.parametrize("tab_name", ["Булки", "Соусы", "Начинки"])
-def test_constructor_tabs(driver, tab_name):
-    driver.get('https://stellarburgers.education-services.ru/')
+class TestConstructorTabs:
 
-    # Поиск вкладки и клик через JavaScript
-    tab = driver.find_element(By.XPATH, f".//span[text()='{tab_name}']")
-    driver.execute_script("arguments[0].click();", tab)
+    @pytest.mark.parametrize("tab_name", ["Булки", "Соусы", "Начинки"])
+    def test_constructor_tabs(self, driver, tab_name):
+        driver.get('https://stellarburgers.education-services.ru/')
 
-    # Проверка, что вкладка активна
-    wait = WebDriverWait(driver, 3)
-    active_tab = wait.until(EC.visibility_of_element_located(
-        (By.XPATH, f".//div[contains(@class, 'tab_tab_type_current')]//span[text()='{tab_name}']")
-    ))
-    assert active_tab.is_displayed()  
+        tab = driver.find_element(By.XPATH, f".//span[text()='{tab_name}']")
+        driver.execute_script("arguments[0].click();", tab)
+
+        wait = WebDriverWait(driver, 3)
+        active_tab = wait.until(EC.visibility_of_element_located(
+            (By.XPATH, f".//div[contains(@class, 'tab_tab_type_current')]//span[text()='{tab_name}']")
+        ))
+        assert active_tab.is_displayed()
