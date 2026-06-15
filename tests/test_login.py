618255b8a0aee246bc998094e4bd36bfe49a66e2
Valentina Loskutova <valentina.losktova@gmail.com>
2026-06-15 23:49:10 +0300

    refactor: use driver fixture in test_login.py

--- tests/test_login.py
+++ tests/test_login.py
@@ -4,8 +4,7 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from helpers import generate_unique_email
 
-def test_login_via_personal_account():
-    driver = webdriver.Chrome()
+def test_login_via_personal_account(driver):
     driver.get('https://stellarburgers.education-services.ru/')
 
     driver.find_element(By.LINK_TEXT, "Личный Кабинет").click()
@@ -14,10 +13,8 @@ def test_login_via_personal_account():
     wait.until(EC.url_contains("login"))
     assert "login" in driver.current_url
 
-    driver.quit()
 
-def test_login_via_main_button():
-    driver = webdriver.Chrome()
+def test_login_via_main_button(driver):
     driver.get('https://stellarburgers.education-services.ru/')
 
     driver.find_element(By.XPATH, "//button[text()='Войти в аккаунт']").click()
@@ -26,10 +23,8 @@ def test_login_via_main_button():
     wait.until(EC.url_contains("login"))
     assert "login" in driver.current_url
 
-    driver.quit()
 
-def test_login_via_registration_form():
-    driver = webdriver.Chrome()
+def test_login_via_registration_form(driver):
     driver.get('https://stellarburgers.education-services.ru/')
 
     driver.find_element(By.LINK_TEXT, "Личный Кабинет").click()
@@ -40,10 +35,8 @@ def test_login_via_registration_form():
     wait.until(EC.url_contains("login"))
     assert "login" in driver.current_url
 
-    driver.quit()
 
-def test_login_via_password_recovery_form():
-    driver = webdriver.Chrome()
+def test_login_via_password_recovery_form(driver):
     driver.get('https://stellarburgers.education-services.ru/')
 
     driver.find_element(By.LINK_TEXT, "Личный Кабинет").click()
@@ -54,6 +47,5 @@ def test_login_via_password_recovery_form():
     wait.until(EC.url_contains("login"))
     assert "login" in driver.current_url
 
-    driver.quit()
     
 
