d5ad4383528a128b46c56935663dea7cbcc757e0
Valentina Loskutova <valentina.losktova@gmail.com>
2026-06-16 00:19:23 +0300

    fix: add missing By import in test_login.py

--- tests/test_login.py
+++ tests/test_login.py
@@ -1,51 +1,36 @@
-from selenium import webdriver
-from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from helpers import generate_unique_email
+from selenium.webdriver.common.by import By
+from pages.locators import StellarLocators
 
 def test_login_via_personal_account(driver):
     driver.get('https://stellarburgers.education-services.ru/')
-
-    driver.find_element(By.LINK_TEXT, "Личный Кабинет").click()
-
+    driver.find_element(*StellarLocators.LOGIN_LINK).click()
     wait = WebDriverWait(driver, 3)
     wait.until(EC.url_contains("login"))
     assert "login" in driver.current_url
-
 
 def test_login_via_main_button(driver):
     driver.get('https://stellarburgers.education-services.ru/')
-
-    driver.find_element(By.XPATH, "//button[text()='Войти в аккаунт']").click()
-
+    driver.find_element(*StellarLocators.LOGIN_BUTTON_MAIN).click()
     wait = WebDriverWait(driver, 3)
     wait.until(EC.url_contains("login"))
     assert "login" in driver.current_url
-
 
 def test_login_via_registration_form(driver):
     driver.get('https://stellarburgers.education-services.ru/')
-
-    driver.find_element(By.LINK_TEXT, "Личный Кабинет").click()
-    driver.find_element(By.LINK_TEXT, "Зарегистрироваться").click()
+    driver.find_element(*StellarLocators.LOGIN_LINK).click()
+    driver.find_element(*StellarLocators.REGISTER_LINK).click()
     driver.find_element(By.LINK_TEXT, "Войти").click()
-
     wait = WebDriverWait(driver, 3)
     wait.until(EC.url_contains("login"))
     assert "login" in driver.current_url
-
 
 def test_login_via_password_recovery_form(driver):
     driver.get('https://stellarburgers.education-services.ru/')
-
-    driver.find_element(By.LINK_TEXT, "Личный Кабинет").click()
-    driver.find_element(By.LINK_TEXT, "Восстановить пароль").click()
+    driver.find_element(*StellarLocators.LOGIN_LINK).click()
+    driver.find_element(*StellarLocators.FORGOT_PASSWORD_LINK).click()
     driver.find_element(By.LINK_TEXT, "Войти").click()
-
     wait = WebDriverWait(driver, 3)
     wait.until(EC.url_contains("login"))
     assert "login" in driver.current_url
-
-    
-
